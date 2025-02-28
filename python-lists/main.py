c4ed50f135f680e27d6ebdc62925870485f244a0
Mswati Tshabalala <mswatitshabalala03@gmail.com>
2025-02-28 13:17:50 +0200

    init

---
+++ python-lists/main.py
@@ -0,0 +1,15 @@
+
+#Dummy code to replace an item in the list
+thislist = ["apple", "banana", "cherry"]
+thislist[2]="carrot"
+print(thislist)
+
+#Dummy code to an item in the end of a  list
+thislist = ["apple", "banana", "cherry"]
+thislist.append("ginger")
+print(thislist)
+
+#Dummy code to insert an item in the   list
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
---
+++ simple-adder/two_number-adder.py
@@ -0,0 +1,6 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+sum = first_number + second_number
+
+print("The sum of the two numbers is : ", sum)
