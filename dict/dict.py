6b1c4300cd3dac511c004682d918456d0dc7cabd
Mswati Tshabalala <mswatitshabalala03@gmail.com>
2025-04-02 23:29:04 +0200

    Add student dictionary and display logic

---
+++ dict/dict.py
@@ -0,0 +1,10 @@
+
+
+students = {
+    "Mswati" : "Maseru",
+    "Thabang" : "Mafeteng",
+    "Thabiso" : "Leribe",
+}
+
+for student in students:
+    print(f"{student} is from {students[student]}")
