86926b86c870fc43d65b65cee64eb70e62b104f4
Mswati Tshabalala <mswatitshabalala03@gmail.com>
2025-04-15 17:02:22 +0200

    Refactor input handling to use get_int function for improved error management

--- exceptions/number.py
+++ exceptions/number.py
@@ -1,9 +1,17 @@
-try:
-    x = int(input("What's x ? "))
-except ValueError:
-    print("x is not an integer")
+def main():
+    x = get_int("What's x ? ")
+    print(f"x is {x}")
 
-print(f"x is {x}")
 
-# this gives a NameError at line 6 because passing a string the "string" and passing it to the int fuction makes no sense hence int function gives the NameError
-# and the x is not equated to anything hence the not defined error.
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("x is not an integer")
+
+    # this gives a NameError at line 6 because passing a string the "string" and passing it to the int fuction makes no sense hence int function gives the NameError
+    # and the x is not equated to anything hence the not defined error.
+
+
+main()
